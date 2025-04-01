6a37e27b3ad272e603367242717680c18ca550c2
daniolbap <chenso-dani@hotmail.com>
2025-03-31 19:18:55 -0600

    Resuelto conflicto de historiales

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let dice1 = Math.floor(Math.random() * 6);
-let dice2 = Math.floor(Math.random() * 6);
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+
+console.log(`${dice1}, ${dice2}`);
+
+function winer() {
+  if (dice1 > dice2) {
+    console.log(`gano dice 1`);
+  } else if (dice2 > dice1) {
+    console.log(`gano dice 2`);
+  } else {
+    console.log(`empate`);
+  }
+}
